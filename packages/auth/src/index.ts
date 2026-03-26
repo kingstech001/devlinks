0defbecd32b9b3f483508a5100893d4a3bc05d80
kingstech001 <mamahkingsleychukwuebuka@gmail.com>
2026-03-26 18:07:06 +0100

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@devlinks/db";
+import * as schema from "@devlinks/db/schema/auth";
+import { env } from "@devlinks/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
